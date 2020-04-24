c371418106c3fbfb5f8409af25fa4c10c0a7479a
Miguelerja <miguelerja@gmail.com>
2020-04-24 16:54:49 +0200

    add first iteration of toggler functionality

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,16 +1,32 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { apiDataContext } from '../../context/api-context';
 import { Loading } from '../Loading/Loading';
+import { Toggler } from '../toggler/Toggler';
+import { Map } from '../map/Map';
+import { DataTable } from '../dataTable/DataTable';
 
 function Container() {
+  const [activeSection, setActiveSection] = useState('map');
   const { data, loading } = useContext(apiDataContext);
-  console.log(data);
-  console.log(loading);
+
+  const renderSection = (activeSection: string) => {
+    if (activeSection === 'map') {
+      return (<Map />)
+    }
+
+    return (<DataTable />)
+  };
+
+  const selectSection = (name: string) => {
+    setActiveSection(name);
+  };
+
   return (
     <div>
+      <Toggler selectSection={selectSection} />
       {loading 
         ? <Loading />
-        : <p>{data[0].lat}</p>
+        : renderSection(activeSection)
       }
     </div>
   );
