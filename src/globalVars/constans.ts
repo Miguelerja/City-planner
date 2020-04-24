0d94cda1a9b2435ba4e685e45e44eb512daeb5d3
Miguelerja <miguelerja@gmail.com>
2020-04-24 19:21:23 +0200

    create module for types and for global variables

---
+++ src/globalVars/constans.ts
@@ -0,0 +1,1 @@
+export const DATA_HEADERS = ['City', 'Description', 'Latitude', 'Longitude', 'Created in'];
---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+interface ApiResponse {
+  id: number;
+  title: string;
+  content: string;
+  lat: string;
+  long: string;
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+  [key: string]: string | number;
+};
+
+type Provider<T> = React.ComponentType<{
+  value: T;
+  children: React.ReactNode;
+}>;
+
+export interface ApiDataProviderProps {
+  children?: React.ReactNode;
+}
+
+export type Data = {loading: boolean, data: Array<ApiResponse>};
