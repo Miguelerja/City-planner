cedc5dcd31073094b52e2ea0b1bd2d084bf2e3d6
Miguelerja <miguelerja@gmail.com>
2020-04-24 15:14:15 +0200

    add first iteration of context for API data

---
+++ src/context/api-context.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { default as api } from '../utils/api';
+
+interface ApiResponse {
+  id: number,
+  title: string,
+  content: string,
+  lat: string,
+  long: string,
+  image_url: string,
+  created_at: string,
+  updated_at: string
+};
+
+type Provider<T> = React.ComponentType<{
+  value: T;
+  children: React.ReactNode;
+}>;
+
+interface ApiDataProviderProps {
+  children?: React.ReactNode;
+}
+
+type Data = {loading: boolean, data: Array<ApiResponse>};
+
+export const apiDataContext = React.createContext<Data>({loading: true, data: []});
+
+export const ApiDataProvider = ({children}: ApiDataProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const data = await api.getPostList();
+    setData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <apiDataContext.Provider value={{loading, data}}>
+      {children}
+    </apiDataContext.Provider>
+  );
+}
