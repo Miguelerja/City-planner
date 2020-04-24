30dd50f4901c6b7a315feb0ff52fce8144be5ee8
Miguelerja <miguelerja@gmail.com>
2020-04-24 16:54:03 +0200

    remove css and minor refactor on App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { ApiDataProvider } from '../context/api-context';
 import Container from './container/container';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ApiDataProvider>
-        <Container />
-      </ApiDataProvider>
-    </div>
+    <ApiDataProvider>
+      <Container />
+    </ApiDataProvider>
   );
 }
 
