f016effd7128b5d2a31535f7b943b0c1179e771c
Miguelerja <miguelerja@gmail.com>
2020-04-24 16:04:33 +0200

    add loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import './loading.scss';
+
+export const Loading = () => {
+  const [loadingText, setLoadingText] = useState('Loading');
+
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      const stopper = 'Loading...';
+      (loadingText === stopper)
+        ? setLoadingText('Loading')
+        : setLoadingText(prevState => (prevState + '.'));
+    }, 500);
+
+    return () => window.clearInterval(interval);
+  }, [loadingText]);
+
+    return (
+      <p className='loading'>{loadingText}</p>
+    );
+};
