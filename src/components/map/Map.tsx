8abea28619a69fc446756650817dce79f722e1dd
Miguelerja <miguelerja@gmail.com>
2020-04-24 21:14:46 +0200

    add external dependencies and remove not needed css

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Map = () => {
+  return (<div>I am in Map</div>);
+};
---
+++ src/components/toggler/Toggler.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import './toggler.scss';
+
+type ToggleProps = { selectSection: (name: string) => void };
+
+export const Toggler = ({ selectSection }: ToggleProps) => {
+  const [activeSection, setActiveSection] = useState('map');
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    selectSection((event.target as any).name);
+    setActiveSection((event.target as any).name);
+  };
+
+  return (
+    <div className='toggler'>
+      <button 
+        onClick={handleClick}
+        className={classNames('btn', { 'active': activeSection === 'map' })}
+        name='map'
+        >City Map</button>
+      <button 
+        onClick={handleClick}
+        className={classNames('btn', { 'active': activeSection === 'list' })}
+        name='list'
+        >City List</button>
+    </div>
+  );
+};
