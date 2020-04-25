689beb00b54d4956b4f4265e2dbcfc407a8bac50
Miguelerja <miguelerja@gmail.com>
2020-04-25 18:01:11 +0200

    fix layout for dataSection

--- src/components/dataCard/dataCard.tsx
+++ src/components/dataCard/dataCard.tsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import classnames from 'classnames';
-import { DATA_HEADERS } from '../../globalVars/constans';
-import { parseDate } from '../../utils/dataManipulation';
 import { ApiPostCall } from '../../types/types';
 import './dataCard.scss'; 
 
@@ -13,20 +11,31 @@ export const DataCard = ({ info }: DataCardProps) => {
   const [isHovered, setIsHovered] = useState<boolean>(false);
   const { title, content, image_url = null } = info;
   
-  const toggleState = () => {
+  const flipCard = () => {
     setIsHovered(prevState => !prevState)
   };
 
   return (
-    <div className="card" onMouseEnter={toggleState} onMouseLeave={toggleState}>
-      {image_url === null 
-        ? <img className={classnames('img', {'hidden': isHovered})} alt={`default landscape`} src={`${process.env.PUBLIC_URL}/pics/default.jpeg`} />
-        : <img className={classnames('img', {'hidden': isHovered})} alt={`${title} representation`} src={image_url} />
-      }
-      
-      <div className={classnames('content', {'hidden': !isHovered})}>
-        <h2>{title}</h2>
-        <p>{content}</p>
+    <div className="card" onMouseEnter={flipCard} onMouseLeave={flipCard}>
+      <div className="card-inner">
+        <div className="card-front">
+          {image_url === null 
+            ? <img 
+                className="img" 
+                alt={`default landscape`} 
+                src={`${process.env.PUBLIC_URL}/pics/default.jpeg`} 
+              />
+            : <img 
+                className="img" 
+                alt={`${title} representation`} 
+                src={image_url} 
+              />
+          }
+        </div>
+        <div className="card-back">
+          <h2 className="name">{title}</h2>
+          <p className="description">{content}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -45,8 +45,9 @@ export const Form = () => {
   const renderForm = () => {
     return (
       <form className="form">
-        <div className="input">          
-          <label className="label" >City name</label>
+        <div>
+          <div className="input">          
+            <label className="label" >City name</label>
             <input 
               className="field" 
               onChange={handleChange} 
@@ -54,30 +55,39 @@ export const Form = () => {
               name="title" 
               required 
             />
-        </div>
-        <div className="input">
-          <label className="label">Latitude</label>
+          </div>
+          <div className="input">
+            <label className="label">Latitude</label>
             <input 
               className="field" 
               onChange={handleChange} 
               type="text" 
               name="latitude" 
             />
-        </div>
-        <div className="input">
-          <label className="label">Longitude</label>
+          </div>
+          <div className="input">
+            <label className="label">Longitude</label>
             <input 
               className="field" 
               onChange={handleChange} 
               type="text" 
               name="longitude" 
             />
+          </div>
         </div>
+
         <div className="input">
           <label className="label">Description</label>
-            <textarea className="textarea" onChange={handleChange} name="content" required />
+          <textarea 
+            className="textarea" 
+            onChange={handleChange} 
+            name="content" 
+            cols={50} 
+            rows={5} 
+            required 
+          />
         </div>
-        <button onClick={handleSubmitForm}>Send</button>
+        <button className="btn-round" onClick={handleSubmitForm}>'\u27940'</button>
       </form>
     );
   };
